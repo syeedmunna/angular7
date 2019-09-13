1f2586774fcd699dbc121862da24545f2aad74f2
Siddu <siddu4u007@gmail.com>
2019-09-13 16:44:21 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use("/", indexRouter);
+//app.use("/", indexRouter);
 // app.use("/users", usersRouter);
 app.use("/employee", employeeRouter);
 
